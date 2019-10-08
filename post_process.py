b8ee92b9b6fd9df59617db59e05025d926c80486
SuYouge <suyouge_bao@qq.com>
2019-10-08 18:41:49 +0800

    Signed-off-by: SuYouge <suyouge_bao@qq.com>

--- post_process.py
+++ post_process.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-import colorList
 import collections
-# filename = 'car04.jpg'
+filename = 'car04.jpg'
 
 
 
@@ -110,7 +109,7 @@ def get_color(frame):
     cntm = []
     for d in color_dict:
         mask = cv2.inRange(hsv, color_dict[d][0], color_dict[d][1])
-        cv2.imwrite(d + '.jpg', mask)
+        cv2.imwrite('cache_pic/' + d + '.jpg', mask)
         binary = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)[1]
         binary = cv2.dilate(binary, None, iterations=2)
         cnts, hiera = cv2.findContours(binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
